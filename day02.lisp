3a7ffb1d22f591b303143b4028c70c4b988fbcb8
Jud Taylor <jud.taylor@protonmail.com>
2020-12-07 12:08:10 -0500

    Day 2, parts 1 & 2

--- day02.lisp
+++ day02.lisp
@@ -44,3 +44,34 @@
 
 
   
+;; part two
+
+(defun parse-password-spec-2 (spec)
+  (destructuring-bind
+      (positions letter password)
+      (cl-utilities:split-sequence #\Space spec)
+    (destructuring-bind
+	(pos1 pos2)
+	(cl-utilities:split-sequence #\- positions)
+      ;; output
+      (list (parse-integer pos1)
+	    (parse-integer pos2)
+	    (coerce (remove #\: letter) 'character)
+	    password))))
+
+(defun correct-freq-p-2 (spec)
+  (destructuring-bind
+      (pos1 pos2 chr password)
+      spec
+    ;; indexing is 1 based
+    ;; password is OK IFF there is one match
+    (= 1 (+ (if (eq chr (char password (- pos1 1))) 1 0)
+	    (if (eq chr (char password (- pos2 1))) 1 0)))))
+
+(defun check-all-passwords-2 (spec-list)
+  (mapcar (lambda (spec)
+	    (correct-freq-p-2 (parse-password-spec-2 spec)))
+	  spec-list))
+
+(defun count-correct-passwords-2 (filename)
+  (count t (check-all-passwords-2 (specs<-file filename))))
